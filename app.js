f26c41159cc9443685c940c0546af5e7596ffead
iMath97 <camusmathias97@gmail.com>
2020-04-29 14:26:37 +0200

    added live reload

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const fetch = require('node-fetch');
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
 
+// livereload
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// app
 const app = express();
+
+app.use(connectLivereload());
 
 // routes
 const weatherRouter = require('./routes/weather');
